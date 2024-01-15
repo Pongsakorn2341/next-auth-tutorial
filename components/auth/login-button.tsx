c8c135e14d8f10b43357be44589ac9761ca155db
Pongsakorn2341 <meepanda2341@gmail.com>
2024-01-15 14:06:30 +0700

    feat: ui card auth

---
+++ components/auth/login-button.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+
+type LoginButtonProps = {
+    children: React.ReactNode,
+    mode?: "modal" | "redirect",
+    asChild?: boolean;
+}
+
+const LoginButton = ({
+    children,
+    mode = 'redirect',
+    asChild
+}: LoginButtonProps) => {
+
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push('/auth/login');
+  }
+
+  if (mode == "modal") {
+    return (
+      <span>
+        TODO: IMPLEMENT MODAL
+      </span>
+    )
+  }
+
+    return (
+    <div>
+        <span onClick={onClick} className='cursor-pointer'>
+            {children}
+        </span>
+    </div>
+  )
+}
+
+export default LoginButton
